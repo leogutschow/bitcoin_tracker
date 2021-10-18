009c34fad28f66f1ec5a24205402693c9c5ee623
leogutschow <73674285+leogutschow@users.noreply.github.com>
2021-10-18 16:53:00 -0300

    Update

--- databaseconnector.py
+++ databaseconnector.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class ConexaoSQL:
     def __init__(self):
         self.conexao = self.conecta()
+        self.criptos = ["Bitcoin", "Ethereum", "Cardano", "Tether", "BinanceCoin", "XRP", "Solana", "Polkadot", "USDCoin", "Dogecoin"]
         print("Conectado com Sucesso!")
 
     def conecta(self):
@@ -31,7 +32,27 @@ class ConexaoSQL:
         dia = datetime.now()
         dia = dia.strftime("%d/%m")
         with self.conexao.cursor() as cursor:
-            cursor.execute('CREATE TABLE IF NOT EXISTS precos')
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS moedas ('
+                    'id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+                    'nome VARCHAR(30) NOT NULL UNIQUE'
+                ')'
+                )
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS precos ('
+                    'id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+                    'dia VARCHAR(30) NOT NULL,'
+                    'preco VARCHAR(30) NOT NULL,'
+                    'moeda INT NOT NULL,'
+                    'FOREIGN KEY (moeda) REFERENCES moedas(id)'
+                ')'
+                )
+            
+            for cripto in self.criptos:
+                insert_ignore = 'INSERT IGNORE INTO moedas (nome) VALUES (%s)'
+
+                cursor.execute(insert_ignore, (cripto))
+                    
             print("Colocando dados na tabela")
             sql = 'INSERT INTO precos (dia, preco, moeda) SELECT  %s, %s, id  FROM moedas WHERE moedas.nome = %s'
             for tupla in lista_tuplas:
--- db/precos.sql
+++
@@ -1,6 +0,0 @@
-select moedas.nome as moeda, precos.dia as dia, 
-precos.preco as preco from moedas, precos
-where moedas.id = precos.moeda
-order by moeda asc;
-
-
